efd8d8bc5e8f0fa67b4ea2856af0e7a951f8edd8
Guilherme-Aguiar <gui9627oli@gmail.com>
2018-04-27 23:29:15 -0300

    implementação da seleção aleatoria de arquivos

--- PROJETO2/eda.c
+++ PROJETO2/eda.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <string.h>
 
 void selecionaImagens();
+int geraNumeroAleatorio();
+void alocaMatriz(int, int);
 
 int main() {
 
@@ -12,9 +16,20 @@ int main() {
   return 0;
 }
 
+
+// int someInt = 368;
+// char str[12];
+// sprintf(str, "%d", someInt);
 void selecionaImagens(){
   FILE *arq;
-  arq = fopen("grass/grass_01.txt","r");
+  int numeroAleatorio;
+  char nomeArquivo[20] = "grass/grass_", stringNumeroAleatorio[4];
+  numeroAleatorio = geraNumeroAleatorio();
+  sprintf(stringNumeroAleatorio,"%02d",numeroAleatorio);
+  strcat(nomeArquivo,stringNumeroAleatorio);
+  strcat(nomeArquivo,".txt");
+  printf("%s\n",nomeArquivo);
+  arq = fopen(nomeArquivo,"r");
   int contLine=0,contCol=0,pixel;
   char aux;
   while(!feof(arq)){
@@ -31,4 +46,9 @@ void selecionaImagens(){
   }
   fclose(arq);
    printf("%d %d",contLine-1,contCol/(contLine-1)+1);
+}
+
+int geraNumeroAleatorio(){
+  srand(time(NULL));
+  return (rand() % 49) + 1;
 }
